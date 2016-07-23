e5e0575828eff9d2d5c8a82d3a9d9763ce0cb323
kingstonkoa <kingston_koa@yahoo.com>
2016-07-24 01:29:23 +0800

    student registeration done (unchecked if exist)

--- enrollmentsystem/WriteStudentToFile.java
+++ enrollmentsystem/WriteStudentToFile.java
@@ -1,7 +1,9 @@
 package enrollmentsystem;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import javax.swing.JOptionPane;
 
 /**
  * @author John
@@ -19,50 +21,99 @@ public class WriteStudentToFile {
 	public static void WriteStudentToFile(String fileName, Student student) {
 		
 		FileWriter fileWriter = null;
+                
+                File f = new File(fileName);
+                if(f.exists() && !f.isDirectory()) {
+                                        try {
+                            fileWriter = new FileWriter(fileName, true);
+
+                            //Write a new student object list to the CSV file
+
+                                    fileWriter.append(String.valueOf(student.getIdNumber()));
+                                    fileWriter.append(COMMA_DELIMITER);
+                                    fileWriter.append(student.getPassword());
+                                    fileWriter.append(COMMA_DELIMITER);
+                                    fileWriter.append(student.getLastName());
+                                    fileWriter.append(COMMA_DELIMITER);
+                                    fileWriter.append(student.getFirstName());
+                                    fileWriter.append(COMMA_DELIMITER);
+                                    fileWriter.append(String.valueOf(student.getMinUnits()));
+                                    fileWriter.append(COMMA_DELIMITER);
+                                    fileWriter.append(String.valueOf(student.getMaxUnits()));
+                                    fileWriter.append(NEW_LINE_SEPARATOR);
+
+
+
+
+                            JOptionPane.showMessageDialog(null,
+							"Student Registration Successfull!");
+
+                    } catch (Exception e) {
+                            JOptionPane.showMessageDialog(null,
+							"Error in registering studnet");
+                            e.printStackTrace();
+                    } finally {
+
+                            try {
+                                    fileWriter.flush();
+                                    fileWriter.close();
+                            } catch (IOException e) {
+                                    System.out.println("Error while flushing/closing fileWriter !!!");
+                    e.printStackTrace();
+                            }
+
+                    }
+                    
+                }
+                else
+                {
 				
-		try {
-			fileWriter = new FileWriter(fileName);
+                    try {
+                            fileWriter = new FileWriter(fileName);
 
-			//Write the CSV file header
-			fileWriter.append(FILE_HEADER);
-			
-			//Add a new line separator after the header
-			fileWriter.append(NEW_LINE_SEPARATOR);
-			
-			//Write a new student object list to the CSV file
-			
-				fileWriter.append(String.valueOf(student.getIdNumber()));
-				fileWriter.append(COMMA_DELIMITER);
-				fileWriter.append(student.getPassword());
-				fileWriter.append(COMMA_DELIMITER);
-				fileWriter.append(student.getLastName());
-				fileWriter.append(COMMA_DELIMITER);
-				fileWriter.append(student.getFirstName());
-				fileWriter.append(COMMA_DELIMITER);
-				fileWriter.append(String.valueOf(student.getMinUnits()));
-                                fileWriter.append(COMMA_DELIMITER);
-				fileWriter.append(String.valueOf(student.getMaxUnits()));
-				fileWriter.append(NEW_LINE_SEPARATOR);
-			
+                            //Write the CSV file header
+                            fileWriter.append(FILE_HEADER);
 
-			
-			
-			System.out.println("file was created successfully !!!");
-			
-		} catch (Exception e) {
-			System.out.println("Error in creating file !!!");
-			e.printStackTrace();
-		} finally {
-			
-			try {
-				fileWriter.flush();
-				fileWriter.close();
-			} catch (IOException e) {
-				System.out.println("Error while flushing/closing fileWriter !!!");
-                e.printStackTrace();
-			}
-			
-		}
+                            //Add a new line separator after the header
+                            fileWriter.append(NEW_LINE_SEPARATOR);
+
+                            //Write a new student object list to the CSV file
+
+                                    fileWriter.append(String.valueOf(student.getIdNumber()));
+                                    fileWriter.append(COMMA_DELIMITER);
+                                    fileWriter.append(student.getPassword());
+                                    fileWriter.append(COMMA_DELIMITER);
+                                    fileWriter.append(student.getLastName());
+                                    fileWriter.append(COMMA_DELIMITER);
+                                    fileWriter.append(student.getFirstName());
+                                    fileWriter.append(COMMA_DELIMITER);
+                                    fileWriter.append(String.valueOf(student.getMinUnits()));
+                                    fileWriter.append(COMMA_DELIMITER);
+                                    fileWriter.append(String.valueOf(student.getMaxUnits()));
+                                    fileWriter.append(NEW_LINE_SEPARATOR);
+
+
+
+
+                            JOptionPane.showMessageDialog(null,
+							"Student Registered Successfully");
+
+                    } catch (Exception e) {
+                            JOptionPane.showMessageDialog(null,
+							"Error in registering student");
+                            e.printStackTrace();
+                    } finally {
+
+                            try {
+                                    fileWriter.flush();
+                                    fileWriter.close();
+                            } catch (IOException e) {
+                                    System.out.println("Error while flushing/closing fileWriter !!!");
+                    e.printStackTrace();
+                            }
+
+                    }
+            }
 	}
 }
 
